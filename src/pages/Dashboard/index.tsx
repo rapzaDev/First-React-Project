38e956c7c171d3f830d30b78398cf672be2057d5
Rafael Pérez <rapzadev@gmail.com>
2020-09-14 20:10:44 -0300

    Dashboard Updated

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on github</Title>
+
+      <Form>
+        <input placeholder="Put the repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/68792232?s=460&u=19e080c9c96db8bc5147069884bec16f258abcd4&v=4"
+            alt="Rafael Pérez"
+          />
+
+          <div>
+            <strong>TwitchX</strong>
+            <p>
+              Recreation of Twitch.tv Interface using React Native & Typescript
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/68792232?s=460&u=19e080c9c96db8bc5147069884bec16f258abcd4&v=4"
+            alt="Rafael Pérez"
+          />
+
+          <div>
+            <strong>TwitchX</strong>
+            <p>
+              Recreation of Twitch.tv Interface using React Native & Typescript
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/68792232?s=460&u=19e080c9c96db8bc5147069884bec16f258abcd4&v=4"
+            alt="Rafael Pérez"
+          />
+
+          <div>
+            <strong>TwitchX</strong>
+            <p>
+              Recreation of Twitch.tv Interface using React Native & Typescript
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
